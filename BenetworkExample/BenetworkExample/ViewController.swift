b5dc65bf4a54005c7f584fa10e0c64f2787546d8
David Elsonbaty <dave@elsonbaty.ca>
2017-10-10 08:22:46 -0700

    Example project

---
+++ BenetworkExample/BenetworkExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BenetworkExample
+//
+//  Created by David Elsonbaty on 10/10/17.
+//  Copyright © 2017 Matterly. All rights reserved.
+//
+
+import UIKit
+import Benetwork
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
