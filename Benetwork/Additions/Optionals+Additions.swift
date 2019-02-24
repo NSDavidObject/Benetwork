4e492572e51f6ee5dadce911b2d33f12eef2ae3d
David Elsonbaty <dave@elsonbaty.ca>
2019-02-24 00:07:43 -0800

    - Cleanup printing

--- Benetwork/Additions/Optionals+Additions.swift
+++ Benetwork/Additions/Optionals+Additions.swift
@@ -5,7 +5,6 @@ extension Optional {
     public func required(file: String = #file, line: Int = #line, function: String = #function) throws -> Wrapped {
         switch self {
         case .none:
-            print("Failed to unwrap required field in \(file.components(separatedBy: "/").last ?? ""):\(line) : \(function)")
             throw ParsingError.valueNotFound
         case .some(let value):
             return value
