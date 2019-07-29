b03984a8c55e527380f30b341e46643446f36a85
David Elsonbaty <NSDavidObject@users.noreply.github.com>
2019-07-28 21:32:49 -0700

    - Update extension access modifier to pulic

--- Benetwork/Additions/Encodable+Additions.swift
+++ Benetwork/Additions/Encodable+Additions.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-extension Encodable {
+public extension Encodable {
   var dictionary: [String: Any]? {
     guard let data = try? JSONEncoder().encode(self) else { return nil }
     return (try? JSONSerialization.jsonObject(with: data, options: .allowFragments)).flatMap { $0 as? [String: Any] }
