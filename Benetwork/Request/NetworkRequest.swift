dd64141f1e4d348cfd053d5a05d179ace2eed886
David Elsonbaty <dave@elsonbaty.ca>
2021-11-28 22:02:13 -0500

    - Add raw data body ability

--- Benetwork/Request/NetworkRequest.swift
+++ Benetwork/Request/NetworkRequest.swift
@@ -1,93 +1,106 @@
 import CommonUtilities
+import SwiftUI
 
 // MARK: - Network Method
 
 public enum NetworkMethod: String {
-    case get = "GET"
-    case put = "PUT"
-    case post = "POST"
-    case delete = "DELETE"
+  case get = "GET"
+  case put = "PUT"
+  case post = "POST"
+  case delete = "DELETE"
+}
+
+public enum NetworkRequestBody {
+  case none
+  case rawData(Data)
+  case paramters([String: AnyObject])
 }
 
 // MARK: - Network Request
 
 public protocol NetworkRequest {
-    var urlBase: String { get }
-    var urlPath: String { get }
-    var method: NetworkMethod { get }
-    var headers: [String: String] { get }
-    var urlParameters: [String: CustomStringConvertible] { get }
-    var bodyParameters: [String: AnyObject] { get }
-    var responseMiddlewares: [NetworkResponseMiddleware.Type] { get }
-    var rateLimiterType: RateLimitType { get }
+  var urlBase: String { get }
+  var urlPath: String { get }
+  var method: NetworkMethod { get }
+  var headers: [String: String] { get }
+  var urlParameters: [String: CustomStringConvertible] { get }
+  var body: NetworkRequestBody { get }
+  var responseMiddlewares: [NetworkResponseMiddleware.Type] { get }
+  var rateLimiterType: RateLimitType { get }
 }
 
 // Setup default values
 extension NetworkRequest {
-    
-    public var urlParameters: [String: CustomStringConvertible] {
-        return [:]
-    }
-    
-    public var bodyParameters: [String: AnyObject] {
-        return [:]
-    }
-    
-    public var responseMiddlewares: [NetworkResponseMiddleware.Type] {
-        return []
-    }
-
-    public var rateLimiterType: RateLimitType {
-        return .none
-    }
+  
+  public var urlParameters: [String: CustomStringConvertible] {
+    return [:]
+  }
+  
+  public var body: NetworkRequestBody {
+    return .none
+  }
+  
+  public var responseMiddlewares: [NetworkResponseMiddleware.Type] {
+    return []
+  }
+  
+  public var rateLimiterType: RateLimitType {
+    return .none
+  }
 }
 
 // Helpers
 extension NetworkRequest {
+  
+  public var urlRequest: URLRequest {
+    let mutableRequest = NSMutableURLRequest(url: constructedURL)
+    mutableRequest.httpMethod = method.rawValue
+    mutableRequest.allHTTPHeaderFields = headers
+    mutableRequest.cachePolicy = .reloadIgnoringLocalCacheData
     
-    public var urlRequest: URLRequest {
-        let mutableRequest = NSMutableURLRequest(url: constructedURL)
-        mutableRequest.httpMethod = method.rawValue
-        mutableRequest.allHTTPHeaderFields = headers
-        mutableRequest.cachePolicy = .reloadIgnoringLocalCacheData
-        
-        if bodyParameters.keys.count > 0, let serializedData = try? JSONSerialization.data(withJSONObject: bodyParameters, options: []) {
-            mutableRequest.httpBody = serializedData
-        }
-
-        guard let request = mutableRequest.copy() as? URLRequest else { fatalError() }
-        return request
+    switch body {
+    case .none: break
+    case .rawData(let data):
+      mutableRequest.httpBody = data
+    case .paramters(let parameters):
+      if parameters.keys.count > 0, let serializedData = try? JSONSerialization.data(withJSONObject: parameters, options: []) {
+        mutableRequest.httpBody = serializedData
+      }
     }
     
-    public var constructedURL: URL {
-        return URL.urlWithURLBase(urlBase, path: urlPath, urlParams: urlParameters)
-    }
+    guard let request = mutableRequest.copy() as? URLRequest else { fatalError() }
+    return request
+  }
+  
+  public var constructedURL: URL {
+    return URL.urlWithURLBase(urlBase, path: urlPath, urlParams: urlParameters)
+  }
 }
 
 // Execution
 extension NetworkRequest {
-    
-    public func rawRequest(completion: @escaping (NetworkResponse<Data>) -> Void) {
-        NetworkHandler.request(self, completion: { completion($0) })
-    }
+  
+  public func rawRequest(completion: @escaping (NetworkResponse<Data>) -> Void) {
+    NetworkHandler.request(self, completion: { completion($0) })
+  }
   
   public func rawRequestOnBackgroundQueue(callbackQueue: DispatchQueue = .main, completion: @escaping (NetworkResponse<Data>) -> Void) {
-      DispatchQueue.global().async {
-        self.rawRequest { result in
-          callbackQueue.async {
-            completion(result)
-          }
+    DispatchQueue.global().async {
+      self.rawRequest { result in
+        callbackQueue.async {
+          completion(result)
         }
       }
-  }
-    
-    public func JSONRequest(completion: @escaping (NetworkResponse<JSON>) -> Void) {
-        rawRequest(completion: { urlDataResponse in
-            let result = urlDataResponse.result
-            let jsonResult = result.flatMap({ JSONSerializer.serialize(data: $0) })
-            let jsonResponse = urlDataResponse.response(withResult: jsonResult)
-            let interceptedJSONResponse = self.responseMiddlewares.intercepting(jsonResponse)
-            completion(interceptedJSONResponse)
-        })
     }
+  }
+  
+  public func JSONRequest(completion: @escaping (NetworkResponse<JSON>) -> Void) {
+    rawRequest(completion: { urlDataResponse in
+      let result = urlDataResponse.result
+      let jsonResult = result.flatMap({ JSONSerializer.serialize(data: $0) })
+      let jsonResponse = urlDataResponse.response(withResult: jsonResult)
+      let interceptedJSONResponse = self.responseMiddlewares.intercepting(jsonResponse)
+      completion(interceptedJSONResponse)
+    })
+  }
 }
