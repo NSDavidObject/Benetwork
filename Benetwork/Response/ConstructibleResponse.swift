bd929cf85be2fed0a3af68517f25a877081510d8
David Elsonbaty <dave@elsonbaty.ca>
2024-05-09 22:16:03 -0500

    - Minor improvements

--- Benetwork/Response/ConstructibleResponse.swift
+++ Benetwork/Response/ConstructibleResponse.swift
@@ -3,52 +3,64 @@ import CommonUtilities
 // MARK: Constructible Response
 
 public protocol ConstructibleResponse {
-    associatedtype ObjectType: JSONConstructible
-    associatedtype ReturnType
-    
-    static func constructResponse(json: Any) throws -> ReturnType
+  associatedtype ObjectType: JSONConstructible
+  associatedtype ReturnType
+  
+  static func constructResponse(json: Any) throws -> ReturnType
 }
 
 extension ConstructibleResponse {
-    
-    public func construct(_ json: JSON) -> Result<ReturnType> {
-        do {
-            return try .success(Self.constructResponse(json: json.value))
-        } catch let error {
-            return .failure(error)
-        }
+  
+  public func construct(_ json: JSON) -> Result<ReturnType> {
+    do {
+      return try .success(Self.constructResponse(json: json.value))
+    } catch let error {
+      return .failure(error)
     }
-
-    public static func constructResponse(json: Any) throws -> ObjectType {
-        guard let jsonDictionary = json as? JSONDictionary else { throw ObjectConstructionError.unexpectedType }
-        return try ObjectType.init(json: jsonDictionary)
-    }
+  }
+  
+  public static func constructResponse(json: Any) throws -> ObjectType {
+    guard let jsonDictionary = json as? JSONDictionary else { throw ObjectConstructionError.unexpectedType }
+    return try ObjectType.init(json: jsonDictionary)
+  }
 }
 
 extension ConstructibleResponse where Self: NetworkRequest {
-    
-    public func requestAndConstruct(withPostConstructionMiddlewares middlewares: [NetworkResponseMiddleware.Type] = [], completion: @escaping (NetworkResponse<ReturnType>) -> Void) {
-        JSONRequest(completion: { jsonResponse in
-            let constructedResult = jsonResponse.result.flatMap({ self.construct($0)  })
-            let constructedResultResponse = jsonResponse.response(withResult: constructedResult)
-            let interceptedConstructedResultResponse = middlewares.intercepting(constructedResultResponse)
-            completion(interceptedConstructedResultResponse)
-        })
-    }
-    
-    public func requestAndConstructOnBackgroundQueue(withPostConstructionMiddlewares middlewares: [NetworkResponseMiddleware.Type] = [], callbackQueue: DispatchQueue = .main, completion: @escaping (NetworkResponse<ReturnType>) -> Void) {
-        DispatchQueue.global().async {
-            self.requestAndConstruct(withPostConstructionMiddlewares: middlewares, completion: { result in
-                callbackQueue.async {
-                    completion(result)
-                }
-            })
+  
+  public func requestAndConstruct(withPostConstructionMiddlewares middlewares: [NetworkResponseMiddleware.Type] = [], completion: @escaping (NetworkResponse<ReturnType>) -> Void) {
+    JSONRequest(completion: { jsonResponse in
+      let constructedResult = jsonResponse.result.flatMap({ self.construct($0)  })
+      let constructedResultResponse = jsonResponse.response(withResult: constructedResult)
+      let interceptedConstructedResultResponse = middlewares.intercepting(constructedResultResponse)
+      completion(interceptedConstructedResultResponse)
+    })
+  }
+  
+  public func requestAndConstructOnBackgroundQueue(withPostConstructionMiddlewares middlewares: [NetworkResponseMiddleware.Type] = [], callbackQueue: DispatchQueue = .main, completion: @escaping (NetworkResponse<ReturnType>) -> Void) {
+    DispatchQueue.global().async {
+      self.requestAndConstruct(withPostConstructionMiddlewares: middlewares, callbackQueue: callbackQueue, completion: { result in
+        callbackQueue.async {
+          completion(result)
         }
+      })
     }
+  }
+  
+  private func requestAndConstruct(withPostConstructionMiddlewares middlewares: [NetworkResponseMiddleware.Type] = [], callbackQueue: DispatchQueue?, completion: @escaping (NetworkResponse<ReturnType>) -> Void) {
+    requestAndConstruct(withPostConstructionMiddlewares: middlewares, completion: { result in
+      if let callbackQueue = callbackQueue {
+        callbackQueue.async {
+          completion(result)
+        }
+      } else {
+        completion(result)
+      }
+    })
+  }
   
   public func requestAndConstruct(withPostConstructionMiddlewares middlewares: [NetworkResponseMiddleware.Type] = []) async throws -> NetworkResponse<ReturnType> {
     try await withCheckedThrowingContinuation { continuation in
-      requestAndConstructOnBackgroundQueue { response in
+      requestAndConstruct(withPostConstructionMiddlewares: middlewares, callbackQueue: nil) { response in
         continuation.resume(returning: response)
       }
     }
