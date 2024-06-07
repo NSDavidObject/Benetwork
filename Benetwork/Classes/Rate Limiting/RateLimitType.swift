46b4a0e612abe7cd7d9918edc1802b60983e1c90
David Elsonbaty <dave@elsonbaty.ca>
2024-06-06 22:34:31 -0500

    - Add rate limiter using token bucket

--- Benetwork/Classes/Rate Limiting/RateLimitType.swift
+++ Benetwork/Classes/Rate Limiting/RateLimitType.swift
@@ -55,6 +55,83 @@ extension RateLimitType {
   }
 }
 
+
+
+
+
+public class TokenBucket: NSObject {
+  public let capacity: Int
+  public private(set) var tokensPerInterval: Int
+  public private(set) var replenishingInterval: TimeInterval
+  
+  private var _tokenCount: Int
+  public var tokenCount: Int {
+    replenish()
+    return _tokenCount
+  }
+  private var lastReplenished: Date
+  
+  public init(capacity: Int, tokensPerInterval: Int, interval: TimeInterval, initialTokenCount: Int = 0) {
+    guard interval > 0.0 else {
+      fatalError("interval must be a positive number")
+    }
+    self.capacity = capacity
+    self.tokensPerInterval = tokensPerInterval
+    self.replenishingInterval = interval
+    self._tokenCount = min(capacity, initialTokenCount)
+    self.lastReplenished = Date()
+  }
+  
+  public func consume(_ count: Int) {
+    guard count <= capacity else {
+      fatalError("Cannot consume \(count) amount of tokens on a bucket with capacity \(capacity)")
+    }
+    
+    let _ = tryConsume(count, until: Date.distantFuture)
+  }
+  
+  public func tryConsume(_ count: Int, until limitDate: Date) -> Bool {
+    guard count <= capacity else {
+      fatalError("Cannot consume \(count) amount of tokens on a bucket with capacity \(capacity)")
+    }
+    
+    return wait(until: limitDate, for: count)
+  }
+  
+  private let condition = NSCondition()
+  private func replenish() {
+    condition.lock()
+    let ellapsedTime = abs(lastReplenished.timeIntervalSinceNow)
+    if ellapsedTime > replenishingInterval {
+      let ellapsedIntervals = Int((floor(ellapsedTime / Double(replenishingInterval))))
+      _tokenCount = min(_tokenCount + (ellapsedIntervals * tokensPerInterval), capacity)
+      lastReplenished = Date()
+      condition.signal()
+    }
+    condition.unlock()
+  }
+  
+  private func wait(until limitDate: Date, for tokens: Int) -> Bool {
+    replenish()
+    
+    condition.lock()
+    defer {
+      condition.unlock()
+    }
+    while _tokenCount < tokens {
+      if limitDate < Date() {
+        return false
+      }
+      DispatchQueue.global().async {
+        self.replenish()
+      }
+      condition.wait(until: Date().addingTimeInterval(0.2))
+    }
+    _tokenCount -= tokens
+    return true
+  }
+}
+
 public class FrequencyRateLimiter {
   public enum Frequency {
     case perMinute
@@ -74,77 +151,67 @@ public class FrequencyRateLimiter {
     case disabled
   }
   
-  private let syncQueue = DispatchQueue(label: "com.watchioapikit.tmdbapi", qos: .userInitiated)
   private let requestsPerInterval: Int
   private lazy var currentRateLimit: Int = requestsPerInterval
   
-  private let interval: TimeInterval // Interval in seconds
-  private var requestTimestamps: [Date] = []
-  
   // Rate limit adaptation
   private var dynamicAdjustState: RateLimitAdjustmentState = .enabled()
+  private let interval: TimeInterval // Interval in seconds
+  
+  private lazy var tokenBucket: Atomic<TokenBucket> = .init(createBucket())
   
   public init(requestsPerFrequency: Int, frequency: Frequency) {
     self.requestsPerInterval = requestsPerFrequency
     self.interval = frequency.intervalInSeconds
   }
   
-  public func execute(_ block: @escaping () -> Void, onQueue queue: DispatchQueue) {
-    syncQueue.async { [weak self] in
-      guard let self = self else { return }
-      
-      let now = Date.now
-      self.cleanupOldRequests(now)
-      
-      if self.requestTimestamps.count >= self.currentRateLimit, let earliestNextRequest = self.requestTimestamps.first?.addingTimeInterval(self.interval) {
-        let delay = max(0, earliestNextRequest.timeIntervalSince(now))
-        syncQueue.asyncAfter(deadline: .now() + delay) {
-          self.execute(block, onQueue: queue) // Retry executing the block after the delay
-        }
-      } else {
-        queue.async { block() }
-        self.requestTimestamps.append(now) // Update request timestamps
-      }
-    }
-  }
-  
-  public func reset() {
-    syncQueue.async { [weak self] in
-      self?.requestTimestamps.removeAll()
-    }
+  public func execute(_ block: @escaping () -> Void, completion: Completion? = nil, onQueue queue: DispatchQueue) {
+    tokenBucket.value.consume(1)
+    queue.async(execute: {
+      block()
+      completion?()
+    })
   }
   
   public func informSuccessfulCompletion() {
-    syncQueue.async { [weak self] in
-      guard let self = self else { return }
-      
-      if case .disabled = dynamicAdjustState {
-        dynamicAdjustState = .enabled()
-      }
+    if case .disabled = dynamicAdjustState {
+      dynamicAdjustState = .enabled()
     }
   }
   
   public func informRateLimitHit() {
-    syncQueue.async { [weak self] in
-      guard let self = self, case .enabled(var rateLimitHits) = dynamicAdjustState else { return }
-      
-      let shouldAdjust: Bool = rateLimitHits % 10 == 0
-      guard shouldAdjust else {
-        dynamicAdjustState = .enabled(hits: rateLimitHits.successor)
-        return
-      }
-      
-      let newLimitDouble: Double = (Double(currentRateLimit) * 0.9).rounded(.down)
-      currentRateLimit = max(1, Int(newLimitDouble))
-      
-      // Disable until next success, this ensures we don't keep decreasing limit during a long rate limit failure buffer.
-      dynamicAdjustState = .disabled
+    guard currentRateLimit != 1 else { return }
+    guard case .enabled(var rateLimitHits) = dynamicAdjustState else { return }
+    
+    let shouldAdjust: Bool = rateLimitHits % 10 == 0
+    guard shouldAdjust else {
+      dynamicAdjustState = .enabled(hits: rateLimitHits.successor)
+      return
     }
+    
+    let newLimitDouble: Double = (Double(currentRateLimit) * 0.9).rounded(.down)
+    currentRateLimit = max(1, Int(newLimitDouble))
+    
+    // Disable until next success, this ensures we don't keep decreasing limit during a long rate limit failure buffer.
+    dynamicAdjustState = .disabled
+    
+    tokenBucket.value = {
+      // Sleep for 15 seconds
+      usleep(useconds_t(interval * 1_000_000)) // Convert 15secs to microseconds
+      return createBucket()
+    }()
   }
   
-  private func cleanupOldRequests(_ currentTime: Date) {
-    requestTimestamps = requestTimestamps.filter { timestamp in
-      return currentTime.timeIntervalSince(timestamp) <= interval
-    }
+  func createBucket() -> TokenBucket {
+    let capacity = currentRateLimit
+    let tokensPerInterval = max(currentRateLimit / Int(interval), 1)
+    let interval = 1
+    return TokenBucket(capacity: capacity, tokensPerInterval: tokensPerInterval, interval: TimeInterval(interval))
+  }
+  
+  func reset() {
+    // Reset
+    currentRateLimit = requestsPerInterval
+    tokenBucket.value = createBucket()
   }
 }
