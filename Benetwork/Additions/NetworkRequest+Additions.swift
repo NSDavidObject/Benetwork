d81beb4a3e78d355fa05956cd804bf198b3a48f1
David Elsonbaty <dave@elsonbaty.ca>
2024-09-04 23:49:15 -0500

    - Cleanups

--- Benetwork/Additions/NetworkRequest+Additions.swift
+++ Benetwork/Additions/NetworkRequest+Additions.swift
@@ -1,11 +1,11 @@
 import Foundation
 
 extension NetworkRequest {
-    
-    public static func jsonHeaders() -> [String: String] {
-        var headers: [String: String] = [:]
-        headers["Accept"] = "application/json"
-        headers["Content-Type"] = "application/json"
-        return headers
-    }
+  
+  public static func jsonHeaders() -> [String: String] {
+    var headers: [String: String] = [:]
+    headers["Accept"] = "application/json"
+    headers["Content-Type"] = "application/json"
+    return headers
+  }
 }
