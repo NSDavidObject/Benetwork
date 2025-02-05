db50219c13f7c32b714aec3bbee931032639d004
David Elsonbaty <dave@elsonbaty.ca>
2025-02-05 15:36:42 -0600

    - error messaging

--- Benetwork/Declarations/ParsingError.swift
+++ Benetwork/Declarations/ParsingError.swift
@@ -1,7 +1,11 @@
 import Foundation
 
 public enum ParsingError: Error {
-    case unexpectedType
-    case unexpectedValue
-    case valueNotFound
+  case unexpectedType
+  case unexpectedValue
+  case valueNotFound
+
+  public var localizedDescription: String {
+    return "Unexpected response".localized
+  }
 }
